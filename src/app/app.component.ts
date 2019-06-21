849d98675d048958bc814467bdf6576f263f2a1d
thakuribipin <thakuribipin4u@gmail.com>
2019-06-21 17:37:04 +0545

    initialised

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TrulyUIBoilerplate';
+
+
+  public dataNormal :any;
+
+  constructor(  ) {
+    this.dataNormal=[
+      {
+        'title':'t',
+        'sdf':'t',
+        'dfg':'t',
+        'dfgfdg':'t',
+        'dfgdfgd':'t',
+      },
+      {
+        'title':'t',
+        'sdf':'t',
+        'dfg':'t',
+        'dfgfdg':'t',
+        'dfgdfgd':'t',
+      }
+    ]
+  }
 }
